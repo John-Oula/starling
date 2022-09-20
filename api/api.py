1ac0943a6c061e72fb9ae8517567009f1a0a0f7e
John-Oula <johnoula@antratechstudios.com>
2022-09-20 09:52:29 +0800

    auth object

--- api/api.py
+++ api/api.py
@@ -1,14 +1,105 @@
+import os
 
-from flask import Flask , request
+import requests
+from flask import Flask, request, url_for, redirect, jsonify
+from requests.auth import HTTPBasicAuth
+from starling import AuthHeader
 
 app = Flask(__name__)
+
+PAT = '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'
+username = 'John-Oula'
+method = 'POST'
+service = 'i18n_openapi'
+host = "open.volcengineapi.com"
+region = 'cn-beijing'
+endpoint = "https://open.volcengineapi.com"
+request_parameters = 'Action=ProjectTaskTextImport&Version=2021-05-21'
+content_type = 'application/json'
+body = '{"projectId":"4894","taskId":"94830089"}'
+access_key = "AKLTMDc3MGY5ZmI4NDI4NDRjZmE0ZjkyMDhjZDQ0YzI0Yzg"
+secret_key = "T0RReE1EQXlZMk0wWVdNMU5ETTBZVGhsTkdFd00yVmxPVGRsWkdRMll6VQ=="
+signed_headers = 'host;x-date'
+canonical_uri = '/'
+canonical_querystring = request_parameters
+algorithm = 'HMAC-SHA256'
 
 # Receive push webhook notifications
 # create project and task
 @app.route("/push",methods=['POST','GET'])
 def push():
-    print(request.json)
-    return '', 200
+
+    response = request.get_json()
+    download_url_list = []
+
+    # Get the commits according to commit type
+
+
+    added_files = response['head_commit']['added']
+    changed_files = response['head_commit']['modified']
+    removed_files = response['head_commit']['removed']
+
+    owner = response['repository']['owner']['name']
+    repo = response['repository']['name']
+
+    # Directory path
+    dir_path = os.path.join(app.root_path +'/projects/repos')
+
+    os.mkdir(os.path.join(dir_path, repo))
+
+
+    # Fetch file(s) download url from github REST API
+    # Using the list of file paths stored in the above variables
+    for path in changed_files:
+        res = requests.get('https://api.github.com/repos/%s/%s/contents/%s' % (owner,repo,path),  auth = HTTPBasicAuth(username, PAT))
+        download_url_list.append(res.json())
+        print(res.json())
+
+    # From the download_url_list
+    # Fetch and Download the files
+    # Save downloaded files
+    for file in download_url_list:
+        res = requests.get(file['download_url'])
+        open(file['name'], "wb").write(res.content)
+        f = open(os.path.join(app.root_path, 'projects/repos/%s' % (repo), file['name']),'wb')
+        f.write(res.content)
+        f.close()
+
+
+
+
+    return '' , 200
+
+@app.route("/auth",methods=['POST','GET'])
+def auth():
+
+    canonical_headers = AuthHeader().canonical_headers(host=host)
+    canonical_request = AuthHeader().canonical_request(method='POST',body=body,canonical_uri=canonical_uri,canonical_querystring=canonical_querystring,signed_headers=signed_headers,host=host)
+    string_to_sign = AuthHeader().string_to_sign(algorithm=algorithm,region=region,service=service)
+    signature =  AuthHeader().signature(secret_key=secret_key,region=region,service=service)
+    auth_header= AuthHeader().generate_auth_header(access_key=access_key,signed_headers=signed_headers,signature=signature)
+    print(auth_header)
+
+    headers = {
+            "content-type": 'application/json',
+            'x-date': AuthHeader().date(),
+            'Authorization': auth_header
+        }
+    request_url = endpoint + '?' + canonical_querystring
+
+    r = requests.post(request_url, headers=auth_header , data=body)
+    print(r.json())
+    return '' , 200
+
+@app.route("/send_file",methods=['POST','GET'])
+def send_file():
+
+    file_path = os.path.abspath('test.json')
+    f = open(file_path, 'wb')
+
+    resp = requests.post(endpoint+'?'+request_parameters,data=body)
+
+    return '' , 200
 
 if __name__ == '__main__':
 
--- starling.py
+++ starling.py
@@ -1,117 +1,123 @@
 import sys, os, base64, datetime, hashlib, hmac
 import requests # pip install requests
 
-# ************* REQUEST VALUES *************
-method = 'POST'
-service = 'i18n_openapi'
-host = "open.volcengineapi.com"
-region = 'cn-beijing'
-endpoint = "https://open.volcengineapi.com"
-request_parameters = 'Action=DocumentCreate&Version=2021-05-21'
-content_type = 'application/json'
-body = '{"projectName":"API test success"}'
 
-def sign(key, msg):
-    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+class AuthHeader(object):
+    def __init__(self):
+        self.algorithm = 'HMAC-SHA256'
+    def sign(self,sk, msg):
+        return hmac.new(sk, msg.encode('utf-8'), hashlib.sha256).digest()
 
-def getSignatureKey(key, dateStamp, regionName, serviceName):
-    kDate = sign((  key).encode('utf-8'), dateStamp)
-    kRegion = sign(kDate, regionName)
-    kService = sign(kRegion, serviceName)
-    kSigning = sign(kService, 'request')
-    return kSigning
+    def getSignatureKey(self,sk, dateStamp, regionName, serviceName):
+        kDate = self.sign((sk).encode('utf-8'), dateStamp)
+        kRegion = self.sign(kDate, regionName)
+        kService = self.sign(kRegion, serviceName)
+        kSigning = self.sign(kService, 'request')
+        return kSigning
 
+    def date(self):
+        # Create a date for headers and the credential string
+        t = datetime.datetime.utcnow()
+        datestamp = t.strftime('%Y%m%d')  # Date w/o time, used in credential scope
+        return datestamp
 
-access_key = "AKLTMDc3MGY5ZmI4NDI4NDRjZmE0ZjkyMDhjZDQ0YzI0Yzg"
-secret_key = "T0RReE1EQXlZMk0wWVdNMU5ETTBZVGhsTkdFd00yVmxPVGRsWkdRMll6VQ=="
-if access_key is None or secret_key is None:
-    print('No access key is available.')
-    sys.exit()
+    def datestamp(self):
+        # Create a date for headers and the credential string
+        t = datetime.datetime.utcnow()
+        date = t.strftime('%Y%m%dT%H%M%SZ')
+        return  date
 
-# Create a date for headers and the credential string
-t = datetime.datetime.utcnow()
-date = t.strftime('%Y%m%dT%H%M%SZ')
-datestamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+    def canonical_headers(self,host):
 
 
-# ************* TASK 1: CREATE A CANONICAL REQUEST *************
+        # Step 2: Create canonical URI--the part of the URI from domain to query
+        # string (use '/' if no path)
 
 
-# Step 1 is to define the verb (GET, POST, etc.)--already done.
+        # Step 3: Create the canonical query string. In this example (a GET request),
+        # request parameters are in the query string. Query string values must
+        # be URL-encoded (space=%20). The parameters must be sorted by name.
+        # For this example, the query string is pre-formatted in the request_parameters variable.
 
-# Step 2: Create canonical URI--the part of the URI from domain to query
-# string (use '/' if no path)
-canonical_uri = '/'
+        # Step 4: Create the canonical headers and signed headers. Header names
+        # must be trimmed and lowercase, and sorted in code point order from
+        # low to high. Note that there is a trailing \n.
 
-# Step 3: Create the canonical query string. In this example (a GET request),
-# request parameters are in the query string. Query string values must
-# be URL-encoded (space=%20). The parameters must be sorted by name.
-# For this example, the query string is pre-formatted in the request_parameters variable.
-canonical_querystring = request_parameters
+        # Step 5: Create the list of signed headers. This lists the headers
+        # in the canonical_headers list, delimited with ";" and in alpha order.
+        # Note: The request can include any headers; canonical_headers and
+        # signed_headers lists those that you want to be included in the
+        # hash of the request. "Host" and "x-date" are always required.
+        canonical_headers = 'host:' + host + '\n' + 'x-date:' + AuthHeader().datestamp() + '\n'
+        print(AuthHeader().datestamp())
+        return canonical_headers
 
-# Step 4: Create the canonical headers and signed headers. Header names
-# must be trimmed and lowercase, and sorted in code point order from
-# low to high. Note that there is a trailing \n.
-canonical_headers = 'host:' + host + '\n' + 'x-date:' + date + '\n'
+    def canonical_request(self,method,body,canonical_uri,canonical_querystring,signed_headers,host):
+        # Step 6: Create payload hash (hash of the request body content). For GET
+        # requests, the payload is an empty string ("").
 
-# Step 5: Create the list of signed headers. This lists the headers
-# in the canonical_headers list, delimited with ";" and in alpha order.
-# Note: The request can include any headers; canonical_headers and
-# signed_headers lists those that you want to be included in the
-# hash of the request. "Host" and "x-date" are always required.
-signed_headers = 'host;x-date'
+        payload_hash = hashlib.sha256(body.encode('utf-8')).hexdigest()
 
-# Step 6: Create payload hash (hash of the request body content). For GET
-# requests, the payload is an empty string ("").
+        # Step 7: Combine elements to create canonical request
+        canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + self.canonical_headers(host) + '\n' + signed_headers + '\n' + payload_hash
+        return canonical_request
+
+    def string_to_sign(self,algorithm,region,service):
+        # ************* TASK 2: CREATE THE STRING TO SIGN*************
+        # Match the algorithm to the hashing algorithm you use, either SHA-1 or
+        # SHA-256 (recommended)
+        global credential_scope
+        print(self.date())
+
+        credential_scope = AuthHeader().datestamp() + '/' + region + '/' + service + '/' + 'request'
+        string_to_sign = algorithm + '\n' + self.date() + '\n' + credential_scope + '\n'
+        print(AuthHeader().datestamp())
+
+        return  string_to_sign
+
+    def signature(self,secret_key, region, service):
+        # ************* TASK 3: CALCULATE THE SIGNATURE *************
+        # Create the signing key using the function defined above.
+        signing_key = self.getSignatureKey(secret_key, AuthHeader().datestamp(), region, service)
+        print(AuthHeader().datestamp())
+
+        # Sign the string_to_sign using the signing_key
+        signature = hmac.new(signing_key, (self.string_to_sign(self.algorithm,region=region,service=service)).encode('utf-8'), hashlib.sha256).hexdigest()
+        return signature
+
+    def generate_auth_header(self,signature,access_key,signed_headers):
+        # ************* TASK 4: ADD SIGNING INFORMATION TO THE REQUEST *************
+        # The signing information can be either in a query string value or in
+        # a header named Authorization. This code shows how to use a header.
+        # Create authorization header and add to request headers
+        authorization_header = self.algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' + 'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+
+        # The request can include any headers, but MUST include "host", "x-date",
+        # and (for this scenario) "Authorization". "host" and "x-date" must
+        # be included in the canonical_headers and signed_headers, as noted
+        # earlier. Order here is not significant.
+        # Python note: The 'host' header is added automatically by the Python 'requests' library.
+        headers = {
+            "content-type": 'application/json',
+            'x-date': AuthHeader().date(),
+            'Authorization': authorization_header
+        }
+        print(self.date)
+        return headers
+
+        # ************* SEND THE REQUEST *************
+        # request_url = endpoint + '?' + canonical_querystring
 
 
-payload_hash = hashlib.sha256(body.encode('utf-8')).hexdigest()
-
-# Step 7: Combine elements to create canonical request
-canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
 
 
-# ************* TASK 2: CREATE THE STRING TO SIGN*************
-# Match the algorithm to the hashing algorithm you use, either SHA-1 or
-# SHA-256 (recommended)
-algorithm = 'HMAC-SHA256'
-credential_scope = datestamp + '/' + region + '/' + service + '/' + 'request'
-string_to_sign = algorithm + '\n' +  date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
 
-# ************* TASK 3: CALCULATE THE SIGNATURE *************
-# Create the signing key using the function defined above.
-signing_key = getSignatureKey(secret_key, datestamp, region, service)
-
-# Sign the string_to_sign using the signing_key
-signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
-
-
-# ************* TASK 4: ADD SIGNING INFORMATION TO THE REQUEST *************
-# The signing information can be either in a query string value or in
-# a header named Authorization. This code shows how to use a header.
-# Create authorization header and add to request headers
-authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
-
-# The request can include any headers, but MUST include "host", "x-date",
-# and (for this scenario) "Authorization". "host" and "x-date" must
-# be included in the canonical_headers and signed_headers, as noted
-# earlier. Order here is not significant.
-# Python note: The 'host' header is added automatically by the Python 'requests' library.
-headers = {
-    "content-type" : 'application/json',
-  'x-date':date,
-    'Authorization':authorization_header}
-
-
-# ************* SEND THE REQUEST *************
-request_url = endpoint + '?' + canonical_querystring
-
-print('\nBEGIN REQUEST++++++++++++++++++++++++++++++++++++')
-print('Request URL = ' + request_url)
-r = requests.post(request_url, headers=headers , data=body)
-
-print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
-print('Response code: %d\n' % r.status_code)
-print(r.text)
-print(r.json())
-print(r)
+# print('\nBEGIN REQUEST++++++++++++++++++++++++++++++++++++')
+# print('Request URL = ' + request_url)
+# r = requests.post(request_url, headers=headers , data=body)
+#
+# print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
+# print('Response code: %d\n' % r.status_code)
+# print(r.text)
+# print(r.json())
+# print(r)
