605eb5a37983d5108f59e7d893dbb4e858627fae
John-Oula <johnoula@antratechstudios.com>
2022-09-29 14:41:05 +0800

    refactor

--- api/api.py
+++ api/api.py
@@ -33,11 +33,12 @@ OP_ID = 2100225925
 
 app_id = 239753
 # Read the  certificate
-with open(os.path.normpath(os.path.expanduser('../certs/github/private_key.pem')),'r') as cert_file:
+with open(os.path.normpath(os.path.expanduser('../certs/github/private_key.pem')), 'r') as cert_file:
     app_key = cert_file.read()
 
 # Create an GitHub integration instance
-git_integration = GithubIntegration(app_id,app_key)
+git_integration = GithubIntegration(app_id, app_key)
+
 
 # volc_auth = VolcAuth(access_key=access_key, secret_access_key=secret_key, region=region, service=service, host=host)
 
@@ -105,6 +106,15 @@ def datestamp():
     date = t.strftime('%Y%m%dT%H%M%SZ')
     return date
 
+# Check file extension
+def checkFileExtension(arr):
+    l = []
+    for filepath in arr:
+        filepath_split = filepath.split('/')
+        file = filepath_split[len(filepath_split)-1]
+        if file.endswith('.json'):
+            l.append(filepath)
+            return l
 
 # Receive push webhook notifications
 # create project and task
@@ -115,7 +125,10 @@ def datestamp():
 
 @app.route("/push", methods=['POST', 'GET'])
 def push():
+    global changed_files
     response = request.get_json()
+    print(response)
+    res_args = request.args
     content_list_b64 = []
     texts = []  #
     request_texts = []  #
@@ -123,6 +136,7 @@ def push():
     # Get the committed file paths according to commit type
     # Directory path
     dir_path = os.path.join(app.root_path + '/projects/repos')
+
     try:
         added_files = response['head_commit']['added']
         changed_files = response['head_commit']['modified']
@@ -131,41 +145,32 @@ def push():
         owner = response['repository']['owner']['name']
         repo = response['repository']['name']
 
-
-
-    except:
-        pass
-
-
-
-        # Make a directory using the repository name
-    try:
         os.mkdir(os.path.join(dir_path, repo))
+
+
     except:
         pass
 
     # Fetch base64 encoded files from GitHub REST API
     # Using the list of file paths in changed_files
     # Append each json object to  content_list_b64
-    print(changed_files)
     for path in changed_files:
-        res = requests.get('https://api.github.com/repos/%s/%s/contents/%s' % (owner, repo, path),
-                           auth=HTTPBasicAuth(username, PAT))
-        content_list_b64.append(res.json())
 
-    # base64 decode "content" prop
-    # write the contents into the file
-    for file in content_list_b64:
-        file_content = base64.b64decode(file["content"])
-        f = open(os.path.join(app.root_path, 'projects/repos/%s' % (repo), file['name']), 'w')
-        f.write(file_content.decode('utf-8'))
 
-        f.close()
+
+        git = Github(
+            login_or_token=git_integration.get_access_token(git_integration.get_installation(owner, repo).id).token)
+        repository = git.get_repo(f"{owner}/{repo}").get_contents(path=path,ref="main")
+        # filename = repository.name
+        # f = open(os.path.join(app.root_path, 'projects/repos/%s' % (repo), filename), 'w')
+        file_to_string = repository.decoded_content.decode('utf-8')
+        # f.write(file_to_string)
+
+        # f.close()
 
         # Convert bytes to string
         # Convert JSON file to Dict
         # Add to list
-        file_to_string = file_content.decode('utf-8')
         to_dict = json.loads(file_to_string)
         texts.append(to_dict)
 
@@ -178,62 +183,31 @@ def push():
         new_obj["content"] = j
 
         request_texts.append(new_obj)
-
+    print(request_texts)
     ##### AUTH #####
 
-    body = {"projectId": 4918, "taskId": "15120356", "texts": request_texts}
-    t = datetime.datetime.utcnow()
-    date = t.strftime('%Y%m%dT%H%M%SZ')
-    datestamp = t.strftime('%Y%m%d')  # Date w/o time, used in credential scope
+    body = {"projectId": res_args["projectId"], "taskId": res_args["taskId"], "texts": request_texts}
 
     canonical_uri = '/'
 
     canonical_querystring = request_parameters
 
-    canonical_headers = 'host:' + host + '\n' + 'x-date:' + date + '\n'
-
-    signed_headers = 'host;x-date'
-
-    payload_hash = hashlib.sha256(json.dumps(body).encode('utf-8')).hexdigest()
-
-    canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
-
-    algorithm = 'HMAC-SHA256'
-    credential_scope = datestamp + '/' + region + '/' + service + '/' + 'request'
-    string_to_sign = algorithm + '\n' + date + '\n' + credential_scope + '\n' + hashlib.sha256(
-        canonical_request.encode('utf-8')).hexdigest()
-
-    # ************* CALCULATE THE SIGNATURE *************
-
-    signing_key = getSignatureKey(secret_key, datestamp, region, service)
-
-    # Sign the string_to_sign using the signing_key
-    signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
-
-    # ************* ADD SIGNING INFORMATION TO THE REQUEST *************
-
-    authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' + 'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
-    auth_headers = generate_auth_headers('POST', canonical_querystring, body, canonical_uri, secret_key, region,
+    auth_headers = generate_auth_headers('POST', canonical_querystring, body, canonical_uri,
+                                         secret_key, region,
                                          service, access_key, host)
-    headers = {
-        "content-type": 'application/json',
-        'x-date': date,
-        'Authorization': authorization_header}
 
     # ************* SEND THE REQUEST *************
     request_url = endpoint + '?' + canonical_querystring
 
-    r = requests.post(request_url, headers=headers, data=json.dumps(body).encode('utf-8').decode())
-    print(headers)
     re = requests.post(request_url, headers=auth_headers, data=json.dumps(body).encode('utf-8').decode())
-    print(auth_headers)
 
-    print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
-    print(re.status_code)
+    print('\nRESPONSE+++++++++++++')
+    # print(re.status_code)
     print(re.text)
-    print(re.request.body)
+
 
     return '', 200
+
 
 @app.route("/hook", methods=['POST'])
 def hook():
@@ -249,92 +223,26 @@ def hook():
         "content_type": "json"
     }
 
-    git = Github(login_or_token=git_integration.get_access_token(git_integration.get_installation(owner, repo).id).token)
-    repo = git.get_repo(f"{owner}/{repo}").create_hook(name="web",events=['push'], active=True,config=config)
+    git = Github(
+        login_or_token=git_integration.get_access_token(git_integration.get_installation(owner, repo).id).token)
+    repo = git.get_repo(f"{owner}/{repo}").create_hook(name="web", events=['push'], active=True, config=config)
     print(repo)
 
-
-    return '', 200
-
-@app.route("/test", methods=['POST', 'GET'])
-def test():
-    q = 'Action=ProjectDetail&Version=2021-05-21'
-    b = {"projectId": 4918}
-
-    auth = generate_auth_headers('GET', q, b, canonical_uri, secret_key, region, service, access_key, host)
-    print(auth)
-    r = requests.get(endpoint + '?' + q, headers=auth, data=json.dumps(b).encode('utf-8').decode())
-    print(r.content)
-
-    return '', 200
-
-
-@app.route("/auth", methods=['POST', 'GET'])
-def auth():
-    texts = []
-    body = {"projectId": 4894, "taskId": "94830089", "texts": texts}
-    t = datetime.datetime.utcnow()
-    date = t.strftime('%Y%m%dT%H%M%SZ')
-    datestamp = t.strftime('%Y%m%d')  # Date w/o time, used in credential scope
-
-    canonical_uri = '/'
-
-    canonical_querystring = request_parameters
-
-    canonical_headers = 'host:' + host + '\n' + 'x-date:' + date + '\n'
-
-    signed_headers = 'host;x-date'
-
-    payload_hash = hashlib.sha256(json.dumps(body).encode('utf-8')).hexdigest()
-
-    canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
-
-    algorithm = 'HMAC-SHA256'
-    credential_scope = datestamp + '/' + region + '/' + service + '/' + 'request'
-    string_to_sign = algorithm + '\n' + date + '\n' + credential_scope + '\n' + hashlib.sha256(
-        canonical_request.encode('utf-8')).hexdigest()
-
-    # ************* TASK 3: CALCULATE THE SIGNATURE *************
-
-    signing_key = getSignatureKey(secret_key, datestamp, region, service)
-
-    # Sign the string_to_sign using the signing_key
-    signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
-
-    # ************* TASK 4: ADD SIGNING INFORMATION TO THE REQUEST *************
-
-    authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' + 'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
-
-    headers = {
-        "content-type": 'application/json',
-        'x-date': date,
-        'Authorization': authorization_header}
-
-    # ************* SEND THE REQUEST *************
-    request_url = endpoint + '?' + canonical_querystring
-
-    r = requests.post(request_url, headers=headers, data=json.dumps(body))
-
-    print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
-    print(r.status_code)
-
-    print(r.text)
-    print(r.request.body)
     return '', 200
 
 
 @app.route("/publish", methods=['POST', 'GET'])
 def publish():
-    # response_data = request.data
-    #
-    # response = response_data.decode('utf-8')
-    #
-    # json_response = json.loads(response)
-    #
-    #
-    # projectId = json_response["projectId"]
+    response_data = request.data
+
+    response = response_data.decode('utf-8')
+
+    json_response = json.loads(response)
+
+
+    projectId = json_response["projectId"]
     args = request.args.to_dict()
-    projectId = 4918
+    # projectId = 4918
     token_url = 'https://starling-public.zijieapi.com/v3/get_auth_token/%s/%s/%s/%s/' % (
         args['key'], OP_ID, projectId, NSPACE_ID)
 
@@ -353,36 +261,46 @@ def publish():
     # Base64 encode the contents
     # PUT contents back to the repository
     # Merge contents
-    b64_contents = base64.b64encode(json.dumps(contents, ensure_ascii=False).encode('utf-8')).decode()
-    print(contents)
+    # b64_contents = base64.b64encode(json.dumps(contents, ensure_ascii=False).encode('utf-8')).decode()
+    # print(contents)
 
     repo_url = args['repo_url']
     # Get file sha prop for the updated file
     # Generate parameters from the repo_url
     req_sha = requests.get(repo_url, auth=HTTPBasicAuth(username, PAT))
-    owner = repo_url.split('/')[4]
-    repo = repo_url.split('/')[5]
-    path_list = repo_url.split('/')[7:]
-    path = ""
 
-    for i in path_list:
-        i = '/' + i
-        path += i
+    owner = repo_url.split('/')[3]
+    repo = repo_url.split('/')[4]
 
-    data = {
-        "owner": owner,
-        "sha": req_sha.json()['sha'],
-        "repo": repo,
-        "path": path,
-        "message": "%s translations" % locale,
-        "content": b64_contents
+    # path_list = repo_url.split('/')[7:]
+    # path = ""
+    git = Github(
+        login_or_token=git_integration.get_access_token(git_integration.get_installation(owner, repo).id).token)
+    repository = git.get_repo(f"{owner}/{repo}").create_pull(
+                                                title='Translation Strings',
+                                                body='Updated Translation strings',
+                                                base='main',
+                                                head='main',
+                                                draft=False
 
-    }
-    print(data['path'])
+    )
+    # for i in path_list:
+    #     i = '/' + i
+    #     path += i
 
-    res = requests.put(repo_url, auth=HTTPBasicAuth(username, PAT), data=json.dumps(data))
+    # data = {
+    #     "owner": owner,
+    #     "sha": req_sha.json()['sha'],
+    #     "repo": repo,
+    #     "path": path,
+    #     "message": "%s translations" % locale,
+    #     "content": b64_contents
+    #
+    # }
+    # print(data['path'])
 
-    print(res.json())
+    # res = requests.put(repo_url, auth=HTTPBasicAuth(username, PAT), data=json.dumps(data))
+
 
     return '', 200
 
